125c4edc2d40e5f7b5d11b70fcbee941c2204005
Saúl Guzmán <sgs319@hotmail.com>
2018-05-06 03:31:34 -0600

    Domingo 06-may-18 3:30AM
    DataTables mejorado
    Charts agreados
    Html casi completo

--- public/js/students-controller.js
+++ public/js/students-controller.js
@@ -18,6 +18,13 @@ $(document).ready( function () {
         }
     ];
     $("#miTabla").DataTable({
+        dom: 'lBfrtip',
+        buttons: [
+            'copyHtml5',
+            'csvHtml5',
+            'pdfHtml5',
+            'print'
+        ],
         data: data,
         columns: [
             { data: 'name' },
@@ -26,6 +33,112 @@ $(document).ready( function () {
             { data: 'start_date' },
             { data: 'office' },
             { data: 'extn' }
-        ]
+        ],
+       // "scrollX": true,
+        "scrollY": '300',
+        "scrollCollapse": true
+    });
+
+    $("#miTabla2").DataTable({
+        data: data,
+        columns: [
+            { data: 'name' },
+            { data: 'position' },
+            { data: 'salary' },
+            { data: 'start_date' },
+            { data: 'office' },
+            { data: 'extn' }
+        ],
+       // "scrollX": true,
+        "scrollY": '300',
+        "scrollCollapse": true        
+    });
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            responsiveAnimationDuration: 3,
+            maintainAspectRatio: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    var ctx2 = document.getElementById("myChart2").getContext('2d');
+    var myPieChart = new Chart(ctx2,{
+        type: 'pie',
+        data:  {
+            datasets: [{
+                data: [10, 20, 30],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)' 
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1,
+                hoverBackgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                hoverBorderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                hoverBorderWidth: 2
+            }],
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue'
+            ]
+        },
+        options: {
+            responsive: true,
+            responsiveAnimationDuration: 3,
+            maintainAspectRatio: true,
+            cutoutPercentage: 0,
+            rotation: -0.5*Math.PI,
+            circumference: 2*Math.PI,
+            animateRotate: true,
+            animateScale: false
+        }
     });
 });
